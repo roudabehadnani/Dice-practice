fe747f962d44e35ce29607e1b3a3481707dbbe42
roudabehadnani <rodabeh.adnani@gmail.com>
2022-07-08 15:52:44 +0200

    Dice

---
+++ dice-practice/src/main/java/org/example/Dice.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import java.util.Random;
+
+public enum Dice {
+
+    D6(6),
+    D10(10),
+    D20(20),
+    D100(100);
+
+    //Field
+    private final int Maxvalue;
+
+    //constructor
+    Dice(int maxvalue) {
+        Maxvalue = maxvalue;
+    }
+
+    public int getMaxvalue() {
+        return Maxvalue;
+    }
+
+    public int roll(){
+        Random random = new Random();
+        int min = 1;
+        int max = Maxvalue;
+        int randomNum = min + random.nextInt(max);
+        return randomNum;
+    }
+}
---
+++ dice-practice/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println(Dice.D6.roll());
+        System.out.println(Dice.D10.roll());
+        System.out.println(Dice.D20.roll());
+        System.out.println(Dice.D100.roll());
+
+    }
+}
